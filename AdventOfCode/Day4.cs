c65e475a25526008db5b1d1767f97a4e2bacd2e1
Tiago Knoch <tiago.knoch@symphony.is>
2024-12-17 09:24:16 +0100

    Day 04 part 2

--- AdventOfCode/Day4.cs
+++ AdventOfCode/Day4.cs
@@ -13,20 +13,23 @@ public class Day4 : BaseDay
         _matrix = _input.ToList<string>().Select(i => i.ToCharArray()).ToArray();
     }
 
-    public override ValueTask<string> Solve_1() => new($"{Solve_1_v2()}");
+    public override ValueTask<string> Solve_1() => new($"{Solve_1_v1()}");
+    public override ValueTask<string> Solve_2() => new($"{Solve_2_v1()}");
 
-    private int Solve_1_v2()
+    private int Solve_1_v1()
     {
         var result = 0;
         for (int row = 0; row < _matrix.Length; row++) // Iterate rows
         {
             for (int col = 0; col < _matrix[row].Length; col++) // Iterate columns in each row
             {
-                char item = _matrix[row][col];
-                if (item == 'X')
+                if (_matrix[row][col] == 'X')
                 {
+                    // Retrieve all possible strings starting at this coordinate
                     var possibleStrings = GetStringsFromCoordinate(row, col);
-                    result += possibleStrings.Count(i => i == "XMAS" || i == "SAMX");
+
+                    // Count valid matches
+                    result += possibleStrings.Count(str => str is "XMAS" or "SAMX");
                 }
             }
         }
@@ -34,7 +37,28 @@ public class Day4 : BaseDay
         return result;
     }
 
-    public override ValueTask<string> Solve_2() => throw new NotImplementedException();
+    private int Solve_2_v1()
+    {
+        var result = 0;
+        for (int row = 0; row < _matrix.Length; row++) // Iterate rows
+        {
+            for (int col = 0; col < _matrix[row].Length; col++) // Iterate columns in each row
+            {
+                if (_matrix[row][col] == 'A')
+                {
+                    // Retrieve all possible strings starting at this coordinate
+                    var possibleStrings = GetXStringsFromCoordinate(row, col);
+                    if (possibleStrings.Length == 2 && possibleStrings.All(str => str == "MAS" || str == "SAM"))
+                    {
+                        result += 1;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
 
     public IEnumerable<string> GetStringsFromCoordinate(int row, int col, int length = 4)
     {
@@ -73,5 +97,30 @@ public class Day4 : BaseDay
             directions.Add(new string(Enumerable.Range(0, length).Select(i => _matrix[row + i][col + i]).ToArray()));
 
         return directions;
+    }
+
+    private string[] GetXStringsFromCoordinate(int row, int col)
+    {
+        // Validate bounds for the matrix
+        if (row < 1 || row >= _matrix.Length - 1 || col < 1 || col >= _matrix[row].Length - 1)
+            return []; // Return empty if not enough space for an X.
+
+        // Upper-left to bottom-right diagonal (e.g., top-left, center, bottom-right)
+        var diagonal1 = new string(
+        [
+        _matrix[row - 1][col - 1], // Top-left
+        _matrix[row][col],         // Center
+        _matrix[row + 1][col + 1]  // Bottom-right
+    ]);
+
+        // Upper-right to bottom-left diagonal (e.g., top-right, center, bottom-left)
+        var diagonal2 = new string(
+        [
+        _matrix[row - 1][col + 1], // Top-right
+        _matrix[row][col],         // Center
+        _matrix[row + 1][col - 1]  // Bottom-left
+    ]);
+
+        return [diagonal1, diagonal2];
     }
 }
